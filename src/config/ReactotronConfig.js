0624bb0423d019a8ff88494fcd2f8937430fa8dc
Robert Marinho <robstermarinho@gmail.com>
2019-11-10 14:43:51 -0300

    Creating Signin and Signup forms and use unform

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -3,7 +3,7 @@ import { reactotronRedux } from 'reactotron-redux';
 //import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure()
+  const tron = Reactotron.configure({ host: '192.168.0.12', port: 9090 })
     .use(reactotronRedux())
     //.use(reactotronSaga())
     .connect();
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+
+        <button type="submit"> Create Account </button>
+        <Link to="/">I have an account</Link>
+      </Form>
+    </>
+  );
 }
