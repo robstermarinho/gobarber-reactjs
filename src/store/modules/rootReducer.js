6e98eca6648d7cb502ad2a503aa0f3ccffca552f
Robert Marinho <robstermarinho@gmail.com>
2019-11-18 20:32:44 -0300

    User sagas

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
 import auth from './auth/reducer';
+import user from './user/reducer';
 
 // Combine all reducers in a same place
 export default combineReducers({
   auth,
+  user,
 });
